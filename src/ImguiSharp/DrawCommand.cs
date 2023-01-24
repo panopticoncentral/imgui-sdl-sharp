8c2eb6fd0f656708247bb17c2a163f2ee2a0e9a8
Paul Vick <paul@panopticoncentral.net>
2023-01-24 14:34:16 -0800

    More renderer

--- src/ImguiSharp/DrawCommand.cs
+++ src/ImguiSharp/DrawCommand.cs
@@ -1,8 +1,23 @@
-﻿namespace ImguiSharp
+﻿using System.Formats.Asn1;
+
+namespace ImguiSharp
 {
     public readonly unsafe struct DrawCommand
     {
         private readonly Native.ImDrawCmd* _cmd;
+
+        public Rectangle ClipRectangle => new(_cmd->ClipRect);
+
+        public TextureId TextureId => new(_cmd->TextureId);
+
+        public uint VertexOffset => _cmd->VtxOffset;
+
+        public uint IndexOffset => _cmd->IdxOffset;
+
+        public uint ElementCount => _cmd->ElemCount;
+
+        ImDrawCallback UserCallback;      // 4-8  // If != NULL, call the function instead of rendering the vertices. clip_rect and texture_id will be set normally.
+        void* UserCallbackData;  // 4-8  // The draw callback code can access this.
 
         internal DrawCommand(Native.ImDrawCmd* cmd)
         {
---
+++ src/ImguiSharp/Rectangle.cs
@@ -0,0 +1,11 @@
+﻿namespace ImguiSharp
+{
+    public readonly record struct Rectangle(float X1, float Y1, float X2, float Y2)
+    {
+        internal Rectangle(Native.ImVec4 vec) : this(vec.X, vec.Y, vec.Z, vec.W)
+        {
+        }
+
+        internal Native.ImVec4 ToNative() => new(X1, Y1, X2, Y2);
+    }
+}
