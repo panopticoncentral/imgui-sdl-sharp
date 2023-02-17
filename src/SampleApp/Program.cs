a942992ab246896da1e7417122bee7ab36a010bb
Paul Vick <paul@panopticoncentral.net>
2023-02-16 20:50:13 -0800

    Demo update

--- src/SampleApp/Program.cs
+++ src/SampleApp/Program.cs
@@ -13,37 +13,34 @@ using var renderer = Renderer.Create(window, -1, RendererOptions.PresentVSync | 
 
 Imgui.CreateContext();
 var io = Imgui.GetIo();
-
-//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+//io.ConfigOptions |= ConfigOptions.NavEnableKeyboard;     // Enable Keyboard Controls
+//io.ConfigOptions |= ConfigOptions.NavEnableGamepad;      // Enable Gamepad Controls
 
 // Setup Dear ImGui style
 Imgui.StyleColorsDark();
-// Imgui.StyleColorsLight();
+//Imgui.StyleColorsLight();
 
 ImplSdl2.Init(window, renderer);
 ImplSdlRenderer.Init(renderer);
 
 // Load Fonts
-// - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-// - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-// - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-// - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-// - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
+// - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui.PushFont()/PopFont() to select them.
+// - AddFontFromFileTtf() will return the Font so you can store it if you need to select the font among multiple.
+// - If the file cannot be loaded, the function will return null. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+// - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling FontAtlas.Build()/GetTextureDataAsXXXX(), which NewFrame below will call.
 // - Read 'docs/FONTS.md' for more instructions and details.
-// - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-//io.Fonts->AddFontDefault();
-//io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
-//io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-//io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-//io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-//ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-//IM_ASSERT(font != NULL);
+//io.Fonts.AddFontDefault();
+//io.Fonts.AddFontFromFileTtf("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
+//io.Fonts.AddFontFromFileTtf("../../misc/fonts/DroidSans.ttf", 16.0f);
+//io.Fonts.AddFontFromFileTtf("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+//io.Fonts.AddFontFromFileTtf("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+//var font = io.Fonts.AddFontFromFileTtf("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, null, io.Fonts.GetGlyphRangesJapanese());
 
-// Our state
 var showDemoWindow = new State<bool>(true);
 var showAnotherWindow = new State<bool>(false);
 var clearColor = new ImguiSharp.Color(0.45f, 0.55f, 0.60f, 1.00f);
+var f = new State<float>(0.0f);
+var counter = 0;
 
 Window.Closed += (sender, args) =>
 {
@@ -64,33 +61,33 @@ while (application.DispatchEvents())
         Imgui.ShowDemoWindow(showDemoWindow);
     }
 
-#if false
-
-    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
     {
-            static float f = 0.0f;
-            static int counter = 0;
+        Imgui.Begin("Hello, world!");
 
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+        Imgui.Text("This is some useful text.");
+        Imgui.Checkbox("Demo Window", showDemoWindow);      // Edit bools storing our window open/close state
+        Imgui.Checkbox("Another Window", showAnotherWindow);
 
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+        Imgui.Slider("float", f, 0.0f, 1.0f);
+#if false
             ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+#endif
 
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::End();
+        if (Imgui.Button("Button"))
+        {
+            counter++;
         }
+        Imgui.SameLine();
+        Imgui.Text($"counter = {counter}");
 
-        // 3. Show another simple window.
-        if (show_another_window)
+        Imgui.Text($"Application average {1000.0f / Imgui.GetIo().Framerate:F3} ms/frame ({Imgui.GetIo().Framerate:F1} FPS)");
+
+        Imgui.End();
+    }
+
+#if false
+    // 3. Show another simple window.
+    if (show_another_window)
         {
             ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
             ImGui::Text("Hello from another window!");
